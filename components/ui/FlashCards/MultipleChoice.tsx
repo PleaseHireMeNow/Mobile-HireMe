c2e692ef9a404e3de536171ffdee0364cddbaef7
Justin Lawson <jstn.lawson@gmail.com>
2023-11-21 20:46:56 -0600

    card in decent shape

--- components/ui/FlashCards/MultipleChoice.tsx
+++ components/ui/FlashCards/MultipleChoice.tsx
@@ -12,6 +12,7 @@ const MultipleChoice = () => {
   const [backgroundColorB, setBackgroundColorB] = useState('');
   const [backgroundColorC, setBackgroundColorC] = useState('');
   const [backgroundColorD, setBackgroundColorD] = useState('');
+  const [nextButtonPressed, setNextButtonPressed] = useState(false);
 
   const generateRandomData = () => {
     return {
@@ -20,6 +21,7 @@ const MultipleChoice = () => {
       randomAnswerB: faker.lorem.sentence({ min: 1, max: 6 }),
       randomAnswerC: faker.lorem.sentence({ min: 1, max: 6 }),
       randomAnswerD: faker.lorem.sentence({ min: 1, max: 6 }),
+      correctAnswer: faker.lorem.sentence({ min: 10, max: 20 }),
     };
   };
 
@@ -29,8 +31,49 @@ const MultipleChoice = () => {
     randomAnswerB,
     randomAnswerC,
     randomAnswerD,
-  } = useMemo(generateRandomData, []); // useMemo ensures data is generated only once
+    correctAnswer,
+  } = useMemo(generateRandomData, [nextButtonPressed]); // useMemo ensures data is generated only once
 
+  const handleSubmit = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  const handleNextButton = () => {
+    setIsFlipped(!isFlipped);
+    setNextButtonPressed(!nextButtonPressed); // Toggle the state to trigger useMemo recalculation
+    setBackgroundColorA('white');
+    setBackgroundColorB('white');
+    setBackgroundColorC('white');
+    setBackgroundColorD('white');
+  };
+
+  const handleSelectAnswer = (selectAnswer: string) => { //typescript!
+    setSelectAnswer(selectAnswer);
+
+    // Reset all background colors to white
+    setBackgroundColorA('white');
+    setBackgroundColorB('white');
+    setBackgroundColorC('white');
+    setBackgroundColorD('white');
+
+    // Set the background color of the selected answer to yellow
+    switch (selectAnswer) {
+      case 'A':
+        setBackgroundColorA('#ffee87');
+        break;
+      case 'B':
+        setBackgroundColorB('#ffee87');
+        break;
+      case 'C':
+        setBackgroundColorC('#ffee87');
+        break;
+      case 'D':
+        setBackgroundColorD('#ffee87');
+        break;
+      default:
+        break;
+    }
+  };
 
   const styles = StyleSheet.create({
     card: {
@@ -58,35 +101,9 @@ const MultipleChoice = () => {
     },
   });
 
-  const handleSubmit = () => {
-    setIsFlipped(!isFlipped);
-  };
-
-  const handleBackgroundColorA = () => {
-    setSelectAnswer('A');
-    setBackgroundColorA((prevColor) => (prevColor === '#fffd8d' ? 'white' : '#fffd8d'));
-  }
-
-  const handleBackgroundColorB = () => {
-    setSelectAnswer('B');
-    setBackgroundColorB((prevColor) => (prevColor === '#fffd8d' ? 'white' : '#fffd8d'));
-  }
-
-  const handleBackgroundColorC = () => {
-    setSelectAnswer('C');
-    setBackgroundColorC((prevColor) => (prevColor === '#fffd8d' ? 'white' : '#fffd8d'));
-  }
-
-  const handleBackgroundColorD = () => {
-    setSelectAnswer('D');
-    setBackgroundColorD((prevColor) => (prevColor === '#fffd8d' ? 'white' : '#fffd8d'));
-  }
-
-  
-
   return (
     <>
-    <Link
+      <Link
         className="text-lg font-semibold text-blue-500 active:scale-105"
         href="/"
       >
@@ -95,28 +112,24 @@ const MultipleChoice = () => {
       <FlipCard
         style={styles.card}
         flip={isFlipped}
-        // friction={50}
-        // perspective={1000}
+        // friction={50} // higher number = more friction
+        // perspective={1000} // lower number = more flat
         flipHorizontal={true}
         flipVertical={false}
-        //flip={false}
-        clickable={false}
+        clickable={false} 
         //onFlipEnd={(isFlipEnd)=>{console.log('isFlipEnd', isFlipEnd)}}
-        onFlipEnd={() => {
-          console.log('Flip animation has ended');
-        }}
       >
         <View
           style={styles.face}
           className="max-w-[90vw] w-[90vw] p-2 my-2 rounded-lg border-solid border-gray-300 border-[1px] shadow-xl"
         >
-          <View className="max-w-[80vw] w-[80vw] p-2 my-2 absolute top-4 rounded-lg border-solid border-black border-[1px]">
-            <Text>
-              <Text className="text-4xl">😼:</Text>
+          
+            <Text className='absolute top-4 p-2'>
+              <Text className="text-6xl text-center">😼</Text>
               {'\n'}
-              <Text className="text-base">{randomSentence}?</Text>
+              <Text className="text-xl">{randomSentence}?</Text>
             </Text>
-          </View>
+          
           <View className="absolute bottom-20">
             <Pressable
               style={{
@@ -124,8 +137,8 @@ const MultipleChoice = () => {
                 backgroundColor: backgroundColorA,
               }}
               className="max-w-[80vw] w-[80vw] p-2 my-2 rounded-lg border-solid border-black border-[1px]"
-              onPress={handleBackgroundColorA}
-              >
+              onPress={() => handleSelectAnswer('A')}
+            >
               <Text>
                 <Text className="font-bold">A: </Text>
                 <Text>{randomAnswerA}</Text>
@@ -137,8 +150,8 @@ const MultipleChoice = () => {
                 backgroundColor: backgroundColorB,
               }}
               className="max-w-[80vw] w-[80vw] p-2 my-2 rounded-lg border-solid border-black border-[1px]"
-              onPress={handleBackgroundColorB}
-              >
+              onPress={() => handleSelectAnswer('B')}
+            >
               <Text>
                 <Text className="font-bold">B: </Text>
                 <Text>{randomAnswerB}</Text>
@@ -150,21 +163,21 @@ const MultipleChoice = () => {
                 backgroundColor: backgroundColorC,
               }}
               className="max-w-[80vw] w-[80vw] p-2 my-2 rounded-lg border-solid border-black border-[1px]"
-              onPress={handleBackgroundColorC}
-              >
+              onPress={() => handleSelectAnswer('C')}
+            >
               <Text>
                 <Text className="font-bold">C: </Text>
                 <Text>{randomAnswerC}</Text>
               </Text>
             </Pressable>
-            <Pressable 
+            <Pressable
               style={{
                 ...styles.pressable,
                 backgroundColor: backgroundColorD,
               }}
               className="max-w-[80vw] w-[80vw] p-2 my-2 rounded-lg border-solid border-black border-[1px]"
-              onPress={handleBackgroundColorD}
-              >
+              onPress={() => handleSelectAnswer('D')}
+            >
               <Text>
                 <Text className="font-bold">D: </Text>
                 <Text>{randomAnswerD}</Text>
@@ -176,13 +189,27 @@ const MultipleChoice = () => {
 
         <View
           style={styles.back}
-          className="max-w-[90vw] w-[90vw] rounded-lg border-solid border-black border-[1px]"
+          className="max-w-[90vw] w-[90vw] rounded-lg border-solid border-gray-300 border-[1px] shadow-xl"
         >
-          <Text>The Back</Text>
-          <MainButton text="Next" onPress={handleSubmit} />
+          <Text className=" absolute top-4 p-2">
+            <Text className="text-center text-6xl">😺</Text>
+            {'\n'}
+            <Text className="text-center text-4xl">Correct!</Text>
+          </Text>
+
+          <View className="p-4">
+            <Text className="text-xl">{correctAnswer}</Text>
+          </View>
+          <Link
+            className="text-md mt-16 font-semibold text-blue-500 active:scale-105"
+            href="/"
+          >
+            Rate this question
+          </Link>
+          <MainButton text="Next" onPress={handleNextButton} />
         </View>
       </FlipCard>
     </>
   );
-}
+};
 export default MultipleChoice;
