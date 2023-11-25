fd647800595660610d90933948371c9535b26ff5
Justin Lawson <jstn.lawson@gmail.com>
2023-11-25 15:35:34 -0600

    page layout complete

--- app/results.tsx
+++ app/results.tsx
@@ -1,23 +1,37 @@
 import { Text, View, StyleSheet, Pressable } from 'react-native';
 import { Link } from 'expo-router';
-
+import { faker } from '@faker-js/faker';
+import Icon from 'react-native-vector-icons/Ionicons';
+import MainButton from '../components/ui/Buttons/MainButton';
 
 export default function ResultsPage() {
-
-    return (
-        <View className="">
-        <Text className="text-xl">Results</Text>
-        <Link
-            className="text-lg font-semibold text-blue-500 active:scale-105"
-            href="/"
-        >
-            Home
-        </Link>
-        <Text>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non deleniti
-            ut pariatur eos nulla dolore enim officia dolorem possimus blanditiis
-            optio voluptate aut velit distinctio minima sunt eius, animi libero.
-        </Text>
+  return (
+    <View className="justify-center items-center bg-sunglow-300 h-full w-[100vw]">
+      <Link
+        className="text-lg font-semibold text-blue-500 active:scale-105 absolute top-4 left-4"
+        href="/"
+      >
+        Home
+      </Link>
+      <Text className="text-8xl text-center">😼</Text>
+      <View className="flex justify-center items-center w-[90vw] bg-white px-1 py-6 my-2 rounded-lg border-solid black border-[1px] shadow-xl">
+        <Text className="text-3xl text-center">Test Complete!</Text>
+        <View className="relative left-32">
+          <Icon name="share-outline" size={30} />
         </View>
-    );
+
+        <View className="flex flex-row">
+          <View className="px-6 py-8 m-4 rounded-lg border-solid black border-[1px] shadow-xl">
+            <Text className="text-xl">⏲ 9 min</Text>
+          </View>
+          <View className="px-6 py-8 m-4 rounded-lg border-solid black border-[1px] shadow-xl">
+            <Text className="text-xl">🎯 %80</Text>
+          </View>
+        </View>
+      </View>
+      <View className="items-center mt-32">
+        <MainButton text="Continue" onPress={() => alert('just kidding')} />
+      </View>
+    </View>
+  );
 }
