5093ef7e8a02b00443a375aa3eb50e0251025e0f
Quynh Ngan Dao <nguy2884@umn.edu>
2023-12-01 17:06:47 -0600

    cleaned up styling for flashcard and result page

--- app/results.tsx
+++ app/results.tsx
@@ -12,17 +12,17 @@ export default function ResultsPage() {
     <HeaderWrapper>
       <View className="justify-center items-center bottom-24 h-full w-[100vw]">
         <Text className="text-center shadow-sm text-8xl shadow-white"><Image source={require('../assets/images/devdog.png')} style={{ width: 250, height: 200 }} /></Text>
-        <View className="flex justify-center items-center w-[90vw] bg-white px-1 py-6 my-2 rounded-lg border-solid border-gray-500/50 border-[1px] shadow-sm shadow-indigo-500/50">
+        <View className="flex justify-center items-center w-[85vw] bg-white px-1 py-6 my-2 rounded-3xl border-solid border-gray-500/30 border-[1px] shadow-sm shadow-gray-400">
           <Text className="text-3xl text-center">Test Complete!</Text>
           <View className="relative shadow-sm left-32 shadow-indigo-500/50">
             <ShareButton />
           </View>
 
           <View className="flex flex-row">
-            <View className="px-6 py-8 m-4 bg-white rounded-lg border-solid border-gray-500/50 border-[1px] shadow-sm shadow-indigo-500/40">
+            <View className="px-6 py-8 m-4 bg-white rounded-2xl border-solid border-gray-500/30 border-[1px] shadow-sm shadow-indigo-500/40">
               <Text className="text-xl">⏲ {randomMinutesNumber} min</Text>
             </View>
-            <View className="px-6 py-8 m-4 bg-white rounded-lg border-solid border-gray-500/50 border-[1px] shadow-sm shadow-indigo-500/40">
+            <View className="px-6 py-8 m-4 bg-white rounded-2xl border-solid border-gray-500/30 border-[1px] shadow-sm shadow-indigo-500/40">
               <Text className="text-xl">🎯 %{randomPercentNumber}</Text>
             </View>
           </View>
