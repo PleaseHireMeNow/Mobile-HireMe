0bb1f47bc51b3c11a3894c116b6af952125ebe24
Quynh Dao <nguy2884@umn.edu>
2023-12-25 21:56:47 -0600

    fixed sizing

--- components/ui/Buttons/PreviousButton.tsx
+++ components/ui/Buttons/PreviousButton.tsx
@@ -11,8 +11,8 @@ interface ButtonProps {
 export default function PreviousButton({ onPress, text, imageSource }: ButtonProps) {
   return (
     <Pressable onPress={onPress} className="flex justify-center items-center bg-powerpoint-200 shadow-sm shadow-gray-500/50 active:border-[1px] active:border-gray-300 rounded-[100%] w-40 h-40 "> 
-    <Image source={imageSource} className='w-full bg-cover h-1/2'/>
-    <Text className="-m-2 text-sm font-bold text-gray-700 capitalize ">{text}</Text>
+    <Image source={imageSource} className='w-3/4 bg-cover h-1/2'/>
+    <Text className="-m-1 text-sm font-bold text-gray-700 capitalize ">{text}</Text>
     </Pressable>
   );
 }
