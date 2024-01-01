4848fde670df800d8c7b339a0053dbaa564f73c2
Michael Duren <michaeld@michaelduren.com>
2023-12-31 19:33:44 -0600

    update types and apis

--- types/models/Stack.ts
+++ types/models/Stack.ts
@@ -1,4 +1,14 @@
+export interface Topic {
+  difficulty: Difficulty;
+  topic: Stack;
+}
+
 export interface Stack {
   name: string;
+  iconPath: string;
+}
+
+export interface Difficulty {
+  name: 'entry-level' | 'mid-level' | 'expert-level';
   iconPath: string;
 }
